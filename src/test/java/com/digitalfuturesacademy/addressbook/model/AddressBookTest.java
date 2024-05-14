53df7d859f5c4b1a78e4855c76510f08cc4d3ef1
Ben Turner <turner.benjamin88@gmail.com>
2024-05-14 16:42:18 +0100

    Passed: AB34
    should return an empty list where no phone number matches found

--- src/test/java/com/digitalfuturesacademy/addressbook/model/AddressBookTest.java
+++ src/test/java/com/digitalfuturesacademy/addressbook/model/AddressBookTest.java
@@ -90,7 +90,8 @@ public class AddressBookTest {
                     () -> assertEquals(2,testAddressBook.searchContacts(td.SEARCH_TERM_MATCHING_BOTH_CONTACTS_BY_NAME).size()), //AB8
                     () -> assertTrue(testAddressBook.searchContacts(td.SEARCH_TERM_MATCHING_BOTH_CONTACTS_BY_NAME).containsAll(testContacts)), //AB9
                     () -> assertEquals(1,testAddressBook.searchContacts(td.SEARCH_TERM_MATCHING_ONE_CONTACT_BY_PHONE_NUMBER).size()), //AB32
-                    () -> assertTrue(testAddressBook.searchContacts(td.SEARCH_TERM_MATCHING_ONE_CONTACT_BY_PHONE_NUMBER).contains(testContact1)) //AB33
+                    () -> assertTrue(testAddressBook.searchContacts(td.SEARCH_TERM_MATCHING_ONE_CONTACT_BY_PHONE_NUMBER).contains(testContact1)), //AB33
+                    () -> assertEquals(0, testAddressBook.searchContacts(td.SEARCH_TERM_MATCHING_ZERO_CONTACTS_BY_PHONE_NUMBER).size()) //AB34
             );
         }
 
